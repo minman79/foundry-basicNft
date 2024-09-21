21ea18b2e8ebc942e7c4732a0e500c66fd9e6147
minman79 <minman79@protonmail.com>
2024-09-21 17:39:48 +0100

    BasicNft.sol ready to mint DOGs

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -5,5 +5,18 @@ pragma solidity ^0.8.18;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    constructor() ERC721("Dogie", "DOG") {}
+    uint256 private s_tokenCounter;
+    mapping(uint256 tokenId => string tokenUri) private s_tokenIdToUri;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft(string memory tokenUri) public {
+        tokenUri = s_tokenIdToUri[s_tokenCounter]; // MESSED ABOUT WITH THIS AND PLACED IT BACK TO FROM TO SEE WHATS HAPPENS
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory uri) {}
 }
